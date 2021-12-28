70deaca8ce3b06daefffeb82392f39abd018ae94
Mcurly17 <g.alex17@gmail.com>
2021-12-28 11:24:15 +0100

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/kisspng-bender-fan-art-bender-futurama-5b4ceadff1c7f1.4734213215317675199904.png">
+    <HelloWorld msg="The very first Alex Web 3.0 page"/>
+	<a href="https://twitter.com/alexgaboriau">Twitter</a>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
